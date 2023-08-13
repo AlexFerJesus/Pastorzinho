e500528f8b87b5ce471ba1e2ec5ea5914413eab3
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-08-12 23:43:58 -0300

    1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿//Faremos uma musica usando comandos.
+﻿//musica feita em comanos tirada de "never gona give you up"
+Console.Beep(800, 1000);
+//Todas as notas foram feitas usando uma frequencia de 1320(Dó) a 2310(Si)
 Console.Write("ré, "); Console.Beep(1485, 200);
 Console.Write("mi, "); Console.Beep(1650, 200);
 Console.Write("sol, "); Console.Beep(1980, 200);
@@ -30,3 +32,13 @@ Console.Write("ré, "); Console.Beep(1485, 200);
 Thread.Sleep(400);
 Console.Write("lá, "); Console.Beep(2145, 400);
 Console.Write("sol, "); Console.Beep(1980, 400);
+
+//colocarei as notas abaixo.
+//Console.Write("ré, "); Console.Beep(1485, 200);
+//Console.Write("mi, "); Console.Beep(1650, 200);
+//Console.Write("fá, "); Console.Beep(1759, 200);
+//Como da para ver tambem usei um fá sustenido.
+//Console.Write("fá#, "); Console.Beep(1676, 400);
+//Console.Write("sol, "); Console.Beep(1980, 200);
+//Console.Write("lá, "); Console.Beep(2145, 200);
+//Console.Write("si, "); Console.Beep(2310, 200);
